8b3472f581ba98c3e0ba72b55939751f0a8455bd
Your Name Here <joyayako@gmail.com>
2012-11-04 18:11:53 -0500

    Added Admin settings

--- PurplePoster/EventManager/models.py
+++ PurplePoster/EventManager/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
+# Used by the Movie class
+class Actor(models.Model):
+
 
 class Movie(models.Model):
+	rot_ID = models.CharField(max_length=20)
 	name = models.CharField(max_length=40)
-	#Actors: String[]
-	#Producers: String[]
+	#Actors_id: String[]
+	#Producers_id: String[]
 	summary = models.CharField(blank=True, max_length=300)
-	#Posters: URL[]
-	#Trailers: URL[]
-	releaseDate = models.DateTimeField(blank=True)
+	#Posters_id: URL[]
+	releaseDate = models.DateField(blank=True)
 	#movie.merge method, which needs to combine duplicates (models.py)
 	def __unicode__(self):
 		return self.name
@@ -16,35 +19,25 @@ class Movie(models.Model):
 
 
 class PurplePoster(models.Model):
-	alias = models.CharField(blank=False, max_length = 50)
+	alias = models.CharField(blank=False, max_length = 50) 
 	movie = models.ForeignKey(Movie)
 	submitter = models.CharField(max_length=50)    #edit later to point to user intsance
 	startTime = models.DateTimeField(blank=False)
 	endTime = models.DateTimeField(blank=True)
 	location = models.CharField(blank=False, max_length=100)
-	locationLat = models.FloatField(blank=True)
-	locationLon = models.FloatField(blank=True)
+#	locationLat = models.FloatField(blank=True)
+#	locationLon = models.FloatField(blank=True)
 	#Image: Image[]
 	#Comments: Comment[]
 	#submitImage()
-	#GetMovieInfo(Movie movie)
 	def __unicode__(self):
 		return self.alias + ' :: ' + unicode(self.movie)
 
-	def getMovieInfo():
-
-
-#def getAlias(name, company=None):
-	# normalize names
-#	name = ' '.join(name.split())
-#	if company: company = ' '.join(company.split())
+	def getMovieInfo(self):
+		return Movie.objects.get(id=self.movie.id)
 	
-#	try:
-#		return ProjectAlias.objects.get(alias=name, project__company=company)
-#	except NotFoundError:
-#		project = Project(name=name, company=company)
-#		alias = ProjectAlias(alias=name, project=project)
-#		return alias
+
+
 
 #class Shoot(Model):
 #	poster = FileField(blank=True, upload_to='posters/')

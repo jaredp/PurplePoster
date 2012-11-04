0486d72857311ec422e4e84e7e43c48447bc3894
Your Name Here <joyayako@gmail.com>
2012-11-04 18:10:55 -0500

    Updated Models.py

---
+++ PurplePoster/EventManager/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from EventManager.models import PurplePoster
+from EventManager.models import Movie
+
+admin.site.register(PurplePoster)
+admin.site.register(Movie)

c5347d02dc237f76559ab611bfa08b5039321b3d
Maria A Taku <mat2185@columbia.edu>
2012-11-05 15:46:54 -0500

    Updated models.py. Change URL so webpage is ../PurplePoster so it doesn't conflict with the admin site

--- PurplePoster/EventManager/admin.py
+++ PurplePoster/EventManager/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from EventManager.models import PurplePoster
 from EventManager.models import Movie
+from EventManager.models import Actor
+from EventManager.models import Producer
+from EventManager.models import Poster
+from EventManager.models import Image
 
 admin.site.register(PurplePoster)
 admin.site.register(Movie)
+admin.site.register(Actor)
+admin.site.register(Producer)
+admin.site.register(Poster)
+admin.site.register(Image)
--- PurplePoster/EventManager/models.py
+++ PurplePoster/EventManager/models.py
@@ -1,17 +1,48 @@
 from django.db import models
 
+
 # Used by the Movie class
 class Actor(models.Model):
+	actorRot_ID = models.CharField(max_length=20)
+	firstName = models.CharField(max_length=50)
+	lastName = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.firstName + ' ' + self.lastName
 
+# Used by the Movie class
+class Producer(models.Model):
+	producerRot_ID = models.CharField( max_length=20)
+	producerName = models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.producerName
+
+# Used by the Movie class
+class Poster(models.Model):
+	posterURL = models.URLField();			#Consider changing this to FileField instead
+	def __unicode__(self):
+		return self.posterURL
+
+
+# Used by PurplePoster class
+class Image(models.Model):
+	imageURL = models.URLField();			#Consider changing this to FileField instead
+	def __unicode__(self):
+		return self.imageURL
+
+
+
+###########################################################################
+######					Main Class Models  								###
+###########################################################################
 
 class Movie(models.Model):
-	rot_ID = models.CharField(max_length=20)
-	name = models.CharField(max_length=40)
-	#Actors_id: String[]
-	#Producers_id: String[]
-	summary = models.CharField(blank=True, max_length=300)
-	#Posters_id: URL[]
-	releaseDate = models.DateField(blank=True)
+	movieRot_ID = models.CharField(blank=True, null=True, max_length=20)
+	name = models.CharField(max_length=100)
+	actor = models.ManyToManyField(Actor)
+	producer = models.ManyToManyField(Producer)
+	summary = models.CharField(blank=True, null=True, max_length=1000)
+	poster = models.ManyToManyField(Poster)
+	releaseDate = models.DateField(blank=True, null=True)
 	#movie.merge method, which needs to combine duplicates (models.py)
 	def __unicode__(self):
 		return self.name
@@ -19,26 +50,21 @@ class Movie(models.Model):
 
 
 class PurplePoster(models.Model):
-	alias = models.CharField(blank=False, max_length = 50) 
+	alias = models.CharField(max_length = 100) 
 	movie = models.ForeignKey(Movie)
-	submitter = models.CharField(max_length=50)    #edit later to point to user intsance
-	startTime = models.DateTimeField(blank=False)
-	endTime = models.DateTimeField(blank=True)
-	location = models.CharField(blank=False, max_length=100)
-#	locationLat = models.FloatField(blank=True)
-#	locationLon = models.FloatField(blank=True)
-	#Image: Image[]
+	submitter = models.CharField(max_length=100)    #edit later to point to user intsance
+	startTime = models.DateTimeField()
+	endTime = models.DateTimeField(blank=True, null=True)
+	location = models.CharField(max_length=100)
+	locationLat = models.FloatField(blank=True,null=True)
+	locationLon = models.FloatField(blank=True,null=True)
+	image = models.ManyToManyField(Image)
 	#Comments: Comment[]
-	#submitImage()
+	#submitImage()  
 	def __unicode__(self):
 		return self.alias + ' :: ' + unicode(self.movie)
 
 	def getMovieInfo(self):
 		return Movie.objects.get(id=self.movie.id)
 	
-
-
-
-#class Shoot(Model):
-#	poster = FileField(blank=True, upload_to='posters/')
 
